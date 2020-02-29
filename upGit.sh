b8580ce8f35302ae78ea6803d9acd7db5fd55ca8
Javie Orduz <jaorduz@ciencias.unam.mx>
2020-02-28 21:38:05 -0600

    2001-28022020

--- upGit.sh
+++ upGit.sh
@@ -1,43 +1,19 @@
 #!/bin/bash
 
-# UpToGit 0.1
-# Actualiza facilmente tu repositorio Git
-# (CC) 2011 Alfonso Saavedra "Son Link"
-# http://sonlinkblog.blogspot.com
+# upGit.sh
+# Actualiza tu repositorio Git
+# (CC) 2020 Javier Orduz
 # Bajo licencia GNU/GPL
 
-# Modo de uso: copia o mueve este script a /usr/bin o /usr/local/bin y desde el directorio donde se encuentre la copia de un repo git, ejecútalo de esta manera:
-# uptogit <ficheros>
-
-# Comprobamos si el directorio en el que estamos es de un repositorio git
-if [ ! -d '.git' ]; then
-    echo 'Esta carpeta no contiene un repositorio Git'
+# hay un repo
+if [!-d '.git']; then 
+    echo 'No hay un archivo git'
     exit -1
 fi
 
-# Ahora comprobamos si se le paso algun parametro
-if [ $# == 0 ]; then
-    echo "UpToGit: ¡Error! No se le a pasado ningún parámetro"
-    echo "uptogit fichero1 fichero2 ... ficheroN"
-    exit -1
-else
-    # Recorremos los parametros para comprobar si son ficheros o directorios
-    for file in $*; do
-	if [ ! -e $file ]; then
-	    echo "UpToGit: El archivo o directorio $file no existe"
-	    exit -1
-	    fi
-	done
-    
-    # Si llegamos hasta aquí, indicamos a Git los archivos a subir
-    git add $*
-    
-    # Esto nos pedira el mensaje del commit
-    echo "Introduce el mensaje del commit:"
+
+git add .
+    echo "Introduce un mensaje:"
     read TXT
-    git commit -m "$TXT"
-
-    # Y terminamos subiendo los archivos
-    git push origin master
-
-fi
+git commit -m "$TXT"
+git push
