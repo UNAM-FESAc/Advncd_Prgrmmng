a41bc292f0d00181c27934e36a5d6983f1cf93b2
Javie Orduz <jaorduz@ciencias.unam.mx>
2020-03-01 18:34:50 -0600

    2005-01032020

--- upGit.sh
+++ upGit.sh
@@ -5,15 +5,8 @@
 # (CC) 2020 Javier Orduz
 # Bajo licencia GNU/GPL
 
-# hay un repo
-if [!-d '.git']; then 
-    echo 'No hay un archivo git'
-    exit -1
-fi
-
-
 git add .
     echo "Introduce un mensaje:"
-    read TXT
-git commit -m "$TXT"
+    read COMMIT
+git commit -m "$COMMIT"
 git push
