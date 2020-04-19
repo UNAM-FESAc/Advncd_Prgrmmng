92db30b5878c9445048aa3afa299c1d0847c9e3d
Javier Orduz <jaorduz@ciencias.unam.mx>
2020-04-19 11:51:41 -0500

    1002-19042020

--- partIII/01-files.c
+++ partIII/01-files.c
@@ -5,8 +5,8 @@
 int main () 
 {
   FILE * archivo;
-  char * nom[2] = {"licencia0.est","licencia1.est"};
-  archivo = fopen ( nom[0], "a" );
+  char nom[] = "licencia0.est";
+  archivo = fopen ( nom, "a" );
   fclose ( archivo );
   return 0;
 }
